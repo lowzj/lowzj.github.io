452c1b2282ee8b15779c77f19688bf4dbcb68dde
lowzj <zj3142063@gmail.com>
2018-06-27 03:48:29 +0800

    complete

--- haha.js
+++ haha.js
@@ -1,5 +1,66 @@
 // Data retrieved from http://vikjavev.no/ver/index.php?spenn=2d&sluttid=16.06.2015.
 
+function imp(count, des) {
+  return {
+    y: count,
+    name: des,
+    marker:{
+      enabled: true,
+      symbol: "url(./important.svg)"
+    }
+  }
+}
+
+
+HD = [
+  ["2018-05-17", imp(5, '18.05.17 相识')],
+  ["2018-05-18", 3],
+  ["2018-05-19", 24],
+  ["2018-05-20", 4],
+  ["2018-05-21", 7],
+  ["2018-05-22", 8],
+  ["2018-05-23", 0],
+  ["2018-05-24", 0],
+  ["2018-05-25", 0],
+  ["2018-05-26", 0],
+  ["2018-05-27", 0],
+  ["2018-05-28", 0],
+  ["2018-05-29", 0],
+  ["2018-05-30", 0],
+  ["2018-05-31", 0],
+  ["2018-06-01", 0],
+  ["2018-06-02", 0],
+  ["2018-06-03", 0],
+  ["2018-06-04", 0],
+  ["2018-06-05", 0],
+  ["2018-06-06", 0],
+  ["2018-06-07", 0],
+  ["2018-06-08", 0],
+  ["2018-06-09", 0],
+  ["2018-06-10", 0],
+  ["2018-06-11", 0],
+  ["2018-06-12", 0],
+  ["2018-06-13", 0],
+  ["2018-06-14", 0],
+  ["2018-06-15", 0],
+  ["2018-06-16", 0],
+  ["2018-06-17", 0],
+  ["2018-06-18", 0],
+  ["2018-06-19", 0],
+  ["2018-06-20", 0],
+  ["2018-06-21", 0],
+  ["2018-06-22", 0],
+  ["2018-06-23", 0],
+  ["2018-06-24", imp(8, '18.06.24 第一次主动打招呼')],
+  ["2018-06-25", 6],
+  ["2018-06-26", imp(7, '18.06.26 此页诞生')]
+]
+
+var datas = new Array(0);
+HD.forEach(function(d){
+  datas.push(d[1]);
+})
+
 Highcharts.chart('container', {
   chart: {
     type: 'spline',
@@ -9,10 +70,10 @@ Highcharts.chart('container', {
     }
   },
   title: {
-    text: 'Wind speed during two days'
+    text: '哈哈哈^_^张全蛋'
   },
   subtitle: {
-    text: '13th & 14th of February, 2018 at two locations in Vik i Sogn, Norway'
+    text: '富士康1号车间打标流水线熟练工每日心情指数'
   },
   xAxis: {
     type: 'datetime',
@@ -22,85 +83,93 @@ Highcharts.chart('container', {
   },
   yAxis: {
     title: {
-      text: 'Wind speed (m/s)'
+      text: '哈哈数量(次/天)'
     },
     minorGridLineWidth: 0,
     gridLineWidth: 0,
     alternateGridColor: null,
-    plotBands: [{ // Light air
-      from: 0.3,
-      to: 1.5,
-      color: 'rgba(68, 170, 213, 0.1)',
+    plotBands: [{
+      from: 0,
+      to: 2,
+      // color: 'rgba(68, 170, 213, 0.1)',
+      color: '#85929E',
       label: {
-        text: 'Light air',
+        text: '了无生趣',
         style: {
           color: '#606060'
         }
       }
-    }, { // Light breeze
-      from: 1.5,
-      to: 3.3,
+    }, {
+      from: 2,
+      to: 5,
       color: 'rgba(0, 0, 0, 0)',
       label: {
-        text: 'Light breeze',
+        text: '稀松平常',
         style: {
           color: '#606060'
         }
       }
-    }, { // Gentle breeze
-      from: 3.3,
-      to: 5.5,
-      color: 'rgba(68, 170, 213, 0.1)',
-      label: {
-        text: 'Gentle breeze',
-        style: {
-          color: '#606060'
-        }
-      }
-    }, { // Moderate breeze
-      from: 5.5,
-      to: 8,
-      color: 'rgba(0, 0, 0, 0)',
-      label: {
-        text: 'Moderate breeze',
-        style: {
-          color: '#606060'
-        }
-      }
-    }, { // Fresh breeze
-      from: 8,
+    }, {
+      from: 5,
       to: 11,
       color: 'rgba(68, 170, 213, 0.1)',
       label: {
-        text: 'Fresh breeze',
+        text: '笑逐颜开',
         style: {
           color: '#606060'
         }
       }
-    }, { // Strong breeze
+    }, {
       from: 11,
-      to: 14,
-      color: 'rgba(0, 0, 0, 0)',
+      to: 16,
+      // color: 'rgba(0, 0, 0, 0)',
+      color: '#27AE60',
       label: {
-        text: 'Strong breeze',
+        text: '眉飞色舞',
         style: {
           color: '#606060'
         }
       }
-    }, { // High wind
-      from: 14,
-      to: 15,
-      color: 'rgba(68, 170, 213, 0.1)',
+    }, {
+      from: 16,
+      to: 22,
+      // color: 'rgba(68, 170, 213, 0.1)',
+      color: '#F9E79F',
       label: {
-        text: 'High wind',
+        text: '欣喜若狂',
         style: {
           color: '#606060'
+        }
+      }
+    }, {
+      from: 22,
+      to: 27,
+      // color: 'rgba(0, 0, 0, 0)',
+      color: '#AF7AC5',
+      label: {
+        text: '心醉神迷',
+        style: {
+          color: '#606060'
+        }
+      }
+    }, {
+      from: 27,
+      to: 30,
+      // color: 'rgba(68, 170, 213, 0.1)',
+      color: '#EC7063',
+      label: {
+        text: '乐极生悲',
+        style: {
+          color: '606060'
         }
       }
     }]
   },
+  legend: {
+    enabled: false
+  },
   tooltip: {
-    valueSuffix: ' m/s'
+    valueSuffix: ' 次'
   },
   plotOptions: {
     spline: {
@@ -113,24 +182,26 @@ Highcharts.chart('container', {
       marker: {
         enabled: false
       },
-      pointInterval: 3600000, // one hour
-      pointStart: Date.UTC(2018, 1, 13, 0, 0, 0)
+      pointInterval: 3600000*24, // one day
+      pointStart: Date.UTC(2018, 04, 17)
+    },
+    series: {
+      events: {
+        legendItemClick: function(e) { return false; }
+      }
     }
   },
   series: [{
-    name: 'Hestavollane',
-    data: [
-      3.7, 3.3, 3.9, 5.1, 3.5, 3.8, 4.0, 5.0, 6.1, 3.7, 3.3, 6.4,
-      6.9, 6.0, 6.8, 4.4, 4.0, 3.8, 5.0, 4.9, 9.2, 9.6, 9.5, 6.3,
-      9.5, 10.8, 14.0, 11.5, 10.0, 10.2, 10.3, 9.4, 8.9, 10.6, 10.5, 11.1,
-      10.4, 10.7, 11.3, 10.2, 9.6, 10.2, 11.1, 10.8, 13.0, 12.5, 12.5, 11.3,
-      10.1
-    ]
-
+    name: '哈哈',
+    data: datas
   }],
   navigation: {
     menuItemStyle: {
       fontSize: '10px'
     }
+  },
+  credits: {
+    text: 'lowzj',
+    href: 'https://github.com/lowzj'
   }
 });
